e16cfe2f3ce213ecb847b27837840342afe4f1ad
kid <kidiatoliny@gmail.com>
2026-06-27 01:39:49 +0200

    chore(project): initialize node package

---
+++ tsup.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: {
+    index: 'src/index.ts',
+    'express/index': 'src/presentation/express/index.ts',
+    'fastify/index': 'src/presentation/fastify/index.ts',
+    'nest/index': 'src/presentation/nest/index.ts',
+  },
+  format: ['esm', 'cjs'],
+  dts: {
+    entry: {
+      index: 'src/index.ts',
+      'express/index': 'src/presentation/express/index.ts',
+      'fastify/index': 'src/presentation/fastify/index.ts',
+      'nest/index': 'src/presentation/nest/index.ts',
+    },
+  },
+  sourcemap: true,
+  clean: true,
+  target: 'node20',
+});
